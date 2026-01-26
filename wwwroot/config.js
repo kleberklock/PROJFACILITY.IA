bc43140516fda6e5eac0109b711a6d86a8545662
Kleber <klockk27@gmail.com>
2026-01-26 08:18:06 -0300

    atualização

--- wwwroot/config.js
+++ wwwroot/config.js
@@ -2,14 +2,14 @@
 
 // 1. Defina aqui onde o Backend está rodando (Localhost agora)
 const BASE_URL = "http://localhost:5217";
-// const BASE_URL = "https://facility-ia-frg6cqbcggasdhea.centralus-01.azurewebsites.net"; // Use essa só na Azure
+//const BASE_URL = "https://facility-ia-frg6cqbcggasdhea.centralus-01.azurewebsites.net"; // Use essa só na Azure
 
 // 2. Compatibilidade para o Login (que usa CONFIG)
 const CONFIG = {
     API_URL: BASE_URL
 };
 
-// 3. Compatibilidade para o Perfil e Script Novo (que usam API_BASE_URL)
+ //3. Compatibilidade para o Perfil e Script Novo (que usam API_BASE_URL)
 const API_BASE_URL = BASE_URL;
 
 console.log("Facility.IA: Conectado em", BASE_URL);
